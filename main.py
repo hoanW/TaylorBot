3ba346e69a111031acf57f452e163be331ae9429
Hoan Vu <37515049+hoanW@users.noreply.github.com>
2021-06-02 12:18:23 +0200

    Update active subreddits

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ r = praw.Reddit(username = "Taylor_Bot",
 
 
 if __name__ == "__main__":
-    #open file
+    #open answers file
     with open("lyrics_quotes.txt") as f:
         content = f.readlines()
     trigger_answers = [x.strip() for x in content] 
 
     #reply to mentions  
-    for m in r.inbox.stream(): #fetch messages as an iterable
-        try: 
-            if m in r.inbox.mentions() and m in r.inbox.unread(): #if m in mentions and is unread
+    for m in r.inbox.mentions(limit=50): #fetch 50 recent mentions
+      try: 
+            if m in r.inbox.unread(): #if mention is unread
                  m.reply(random.choice(trigger_answers))
                  m.mark_read() #mark as read
         except praw.exceptions.APIException:
             print("I can't function properly right now, maybe because of rate limit?")
 
     #reply to comments
-    # phrases to trigger the bot
-    trigger_phrases = ["!TaylorBot", "!Taylor_Bot", "Taylor Swift", "lyrics"]
+    trigger_phrase = "Taylor Swift" #phrases to trigger the bot
+    COMMENT_FOOTER = "\n\n\n `I am a bot who likes quoting Taylor Swift's song lyrics`"
 
-    subreddit = r.subreddit("TaylorSwift")
-    for comment in subreddit.stream.comments():
+    subreddit = r.subreddit("all")
+    for comment in subreddit.stream.comments(skip_existing = True): #streaming comments from all subreddits
         try:
-            for phrase in trigger_phrases:
-                if phrase in comment.body:
-                    comment.reply(random.choice(trigger_answers))
+            if trigger_phrase in comment.body and comment.author != "Taylor_Bot": #if the comment is not made by this own bot and contains the word Taylor Swift
+                comment.reply(random.choice(trigger_answers) + COMMENT_FOOTER)
+                comment.upvote()
         except praw.exceptions.APIException:
             print("I can't function properly right now, maybe because of rate limit?")
 
